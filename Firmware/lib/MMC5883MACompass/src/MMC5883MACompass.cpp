a4bc7fe82abbe83c5b64976060c76dbd5b9a6281
vjspdhpp <102199324+vjspdhpp@users.noreply.github.com>
2025-08-05 17:13:42 +0800

    Update MMC5883MACompass.cpp

--- Firmware/lib/MMC5883MACompass/src/MMC5883MACompass.cpp
+++ Firmware/lib/MMC5883MACompass/src/MMC5883MACompass.cpp
@@ -50,8 +50,7 @@ void MMC5883MACompass::setSmoothing(byte steps, bool adv) {
 }
 
 void MMC5883MACompass::calibrate() {
-  setCalibrationOffsets(0., 0., 0.);
-  setCalibrationScales(1., 1., 1.);
+  clearCalibration();
   long calibrationData[3][2] = {
       {65000, -65000}, {65000, -65000}, {65000, -65000}};
   
@@ -97,13 +96,40 @@ void MMC5883MACompass::calibrate() {
                  calibrationData[2][0], calibrationData[2][1]);
 }
 
-void MMC5883MACompass::setCalibration(int x_min, int x_max, int y_min, int y_max, int z_min, int z_max) {
-    _offset[0] = (x_max + x_min) / 2.0;
-    _offset[1] = (y_max + y_min) / 2.0;
-    _offset[2] = (z_max + z_min) / 2.0;
-    _scale[0]  = 2.0 / (x_max - x_min);
-    _scale[1]  = 2.0 / (y_max - y_min);
-    _scale[2]  = 2.0 / (z_max - z_min);
+void MMC5883MACompass::setCalibration(int x_min, int x_max, int y_min, int y_max,
+                                     int z_min, int z_max) {
+  setCalibrationOffsets((x_min + x_max) / 2.0, (y_min + y_max) / 2.0,
+                        (z_min + z_max) / 2.0);
+
+  float x_avg_delta = (x_max - x_min) / 2.0;
+  float y_avg_delta = (y_max - y_min) / 2.0;
+  float z_avg_delta = (z_max - z_min) / 2.0;
+
+  float avg_delta = (x_avg_delta + y_avg_delta + z_avg_delta) / 3.0;
+
+  setCalibrationScales(avg_delta / x_avg_delta, avg_delta / y_avg_delta,
+                       avg_delta / z_avg_delta);
+}
+
+// (No change in logic)
+void MMC5883MACompass::setCalibrationOffsets(float x_offset, float y_offset,
+                                            float z_offset) {
+  _offset[0] = x_offset;
+  _offset[1] = y_offset;
+  _offset[2] = z_offset;
+}
+
+// (No change in logic)
+void MMC5883MACompass::setCalibrationScales(float x_scale, float y_scale,
+                                           float z_scale) {
+  _scale[0] = x_scale;
+  _scale[1] = y_scale;
+  _scale[2] = z_scale;
+}
+
+void MMC5883MACompass::clearCalibration() {
+  setCalibrationOffsets(0., 0., 0.);
+  setCalibrationScales(1., 1., 1.);
 }
 
 void MMC5883MACompass::read() {
